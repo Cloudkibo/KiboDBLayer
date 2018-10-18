9aa5b3dc0c963a945b925cd10871de66a109a982
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-10-18 17:58:30 +0500

    solution for #3

---
+++ server/api/v1/kiboengage/automation_queue/validationSchema.js
@@ -0,0 +1,106 @@
+/*
+This file will contain the validation schemas.
+By separating it from controller, we are cleaning the code.
+Now the middleware will automatically send error response if the payload fails
+*/
+// For express json validation
+exports.createPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'automatedMessageId': {
+      'type': 'string'
+    },
+    'subscriberId': {
+      'type': 'string'
+    },
+    'companyId': {
+      'type': 'string'
+    },
+    'type': {
+      'type': 'string'
+    },
+    'scheduledTime': {
+      'type': 'string'
+    }
+  },
+  'required': [
+    'automatedMessageId',
+    'subscriberId',
+    'companyId',
+    'type',
+    'scheduledTime'
+  ]
+}
+
+exports.queryPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string'
+    },
+    'match': {
+      'type': 'object',
+      'properties': {
+        'automatedMessageId': {
+          'type': 'string'
+        },
+        'subscriberId': {
+          'type': 'string'
+        },
+        'companyId': {
+          'type': 'string'
+        },
+        'type': {
+          'type': 'string'
+        },
+        'scheduledTime': {
+          'type': 'string'
+        }
+      }
+    }
+  },
+  'required': [
+    'purpose',
+    'match'
+  ]
+}
+
+exports.updatePayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string'
+    },
+    'match': {
+      'type': 'object',
+      'properties': {
+        'automatedMessageId': {
+          'type': 'string'
+        },
+        'subscriberId': {
+          'type': 'string'
+        },
+        'companyId': {
+          'type': 'string'
+        },
+        'type': {
+          'type': 'string'
+        },
+        'scheduledTime': {
+          'type': 'string'
+        }
+      }
+    },
+    'updated': {
+      'type': 'object'
+    }
+  },
+  'required': [
+    'purpose',
+    'match',
+    'updated'
+  ]
+}
