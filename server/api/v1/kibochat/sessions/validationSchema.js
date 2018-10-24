af662670b2a61c3149132ca901560aed2a7d2a26
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-10-24 11:16:16 +0500

    solution for #17

---
+++ server/api/v1/kibochat/sessions/validationSchema.js
@@ -0,0 +1,107 @@
+/*
+This file will contain the validation schemas.
+By separating it from controller, we are cleaning the code.
+Now the middleware will automatically send error response if the payload fails
+*/
+// For express json validation
+exports.createPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'subscriber_id': {
+      'type': 'string'
+    },
+    'page_id': {
+      'type': 'string'
+    },
+    'company_id': {
+      'type': 'string'
+    },
+    'assigned_to': {
+      'type': 'object'
+    },
+    'agent_activity_time': {
+      'type': 'string'
+    }
+  },
+  'required': [
+    'subscriber_id',
+    'page_id',
+    'company_id',
+    'assigned_to',
+    'agent_activity_time'
+  ]
+}
+
+exports.queryPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string'
+    },
+    'match': {
+      'type': 'object',
+      'properties': {
+        'subscriber_id': {
+          'type': 'string'
+        },
+        'page_id': {
+          'type': 'string'
+        },
+        'company_id': {
+          'type': 'string'
+        },
+        'assigned_to': {
+          'type': 'object'
+        },
+        'agent_activity_time': {
+          'type': 'string'
+        }
+      }
+    }
+  },
+  'required': [
+    'purpose',
+    'match'
+  ]
+}
+
+exports.updatePayload =
+{
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string'
+    },
+    'match': {
+      'type': 'object',
+      'properties': {
+        'subscriber_id': {
+          'type': 'string'
+        },
+        'page_id': {
+          'type': 'string'
+        },
+        'company_id': {
+          'type': 'string'
+        },
+        'assigned_to': {
+          'type': 'object'
+        },
+        'agent_activity_time': {
+          'type': 'string'
+        }
+      }
+    },
+    'updated': {
+      'type': 'object'
+    }
+  },
+  'required': [
+    'purpose',
+    'match',
+    'updated'
+  ]
+}
