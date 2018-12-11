90a66828aa5b389cef5dba82d03d7b5ab3ac603c
Imran Shoukat <ishoukat@localhost.localdomain>
2018-12-11 17:07:31 +0500

    script

--- server/api/v1/scripts/controller.js
+++ server/api/v1/scripts/controller.js
@@ -1,19 +1,19 @@
-const NotificationsDataLayer = require('../kibochat/notifications/notifications.datalayer')
+const NotificationsModel = require('../kibochat/notifications/notifications.model')
 const TAG = '/api/v1/scripts/controller.js'
 
 exports.normalizeKiboChat = function (req, res) {
   console.log(TAG, `normalizeKiboChat endpoint is hit:`)
   res.status(200).json({status: 'success'})
-  NotificationsDataLayer.findNotificationUsingQuery({purpose: 'findAll', match: {agentId: {$type: 7}, companyId: {$type: 7}}})
+  NotificationsModel.find({agentId: {$type: 7}, companyId: {$type: 7}})
     .then(notifications => {
       notifications.forEach(notification => {
         /* eslint-disable */
         notification.agentId = new String(notification.agentId)
         notification.companyId = new String(notification.companyId)
         /* eslint-enable */
-        NotificationsDataLayer.deleteNotification({purpose: 'deleteOne', match: {_id: notification._id}})
+        NotificationsModel.deleteOne({_id: notification._id})
           .then(deleted => {
-            notification.insert((err, saved) => {
+            NotificationsModel.insert((err, saved) => {
               if (err) console.log(TAG, `Failed to save notification ${err}`)
             })
           })
