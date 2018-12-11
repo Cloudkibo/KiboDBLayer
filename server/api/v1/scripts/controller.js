ed8b78842e45493ba33015b9a6a00d0e49eec3cf
Imran Shoukat <ishoukat@localhost.localdomain>
2018-12-11 16:21:04 +0500

    script

--- server/api/v1/scripts/controller.js
+++ server/api/v1/scripts/controller.js
@@ -1,23 +1,15 @@
-const NotificationsDataLayer = require('../kibochat/notifications/notifications.datalayer')
+const NotificationsModel = require('../kibochat/notifications/notifications.model')
 const TAG = '/api/v1/scripts/controller.js'
 
 exports.normalizeKiboChat = function (req, res) {
   console.log(TAG, `normalizeKiboChat endpoint is hit:`)
   res.status(200).json({status: 'success'})
-  NotificationsDataLayer.findAllNotificationObjects()
-    .then(notifications => {
-      notifications.forEach(notification => {
-        /* eslint-disable */
-        notification.agentId = new String(notification.agentId)
-        notification.companyId = new String(notification.companyId)
-        /* eslint-enable */
-        notification.save((err, saved) => {
-          if (err) console.log(TAG, `Failed to save notification ${err}`)
-          else console.log(TAG, `Notification saved ${saved}`)
-        })
-      })
-    })
-    .catch(err => {
-      console.log(TAG, `Failed to normalize notifications data ${err}`)
+  NotificationsModel.find({agentId: {$type: 7}, companyId: {$type: 7}})
+    .forEach(notification => {
+      /* eslint-disable */
+      notification.agentId = new String(notification.agentId)
+      notification.companyId = new String(notification.companyId)
+      /* eslint-enable */
+      NotificationsModel.save(notification)
     })
 }
