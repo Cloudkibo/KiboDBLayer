2f521894c552faf87ac78446521c30e574104af8
Imran Shoukat <ishoukat@192.168.8.100>
2018-12-21 14:10:42 +0500

    date comparison

--- server/api/v1/kiboengage/broadcasts/broadcasts.logiclayer.js
+++ server/api/v1/kiboengage/broadcasts/broadcasts.logiclayer.js
@@ -26,8 +26,13 @@ exports.validateCreatePayload = (body) => {
 exports.prepareMongoAggregateQuery = (body) => {
   let query = []
 
-  if (body.match) query.push({$match: body.match})
-  else return 'Match Criteria Not Found'
+  if (body.match) {
+    if (body.match.datetime && body.match.datetime.$gte && body.match.datetime.$lt) {
+      body.match.datetime.$gte = new Date(body.match.datetime.$gte)
+      body.match.datetime.$lt = new Date(body.match.datetime.$lt)
+      query.push({$match: body.match})
+    } else query.push({$match: body.match})
+  } else return 'Match Criteria Not Found'
 
   if (body.group) {
     if (!Object.keys(body.group).includes('_id')) return '_id is missing in Group Criteria'
