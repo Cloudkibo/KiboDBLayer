77edb0d68f99536030886457719d35274a8b0fdf
Baqar <bjafri5@gmail.com>
2018-12-28 12:03:14 +0500

    fix imports

--- server/api/v1/kiboengage/surveys/surveys.logiclayer.js
+++ server/api/v1/kiboengage/surveys/surveys.logiclayer.js
@@ -3,6 +3,8 @@ This file will contain the functions for logic layer.
 By separating it from controller, we are separating the concerns.
 Thus we can use it from other non express callers like cron etc
 */
+const mongoose = require('mongoose')
+
 exports.validateCreatePayload = (body) => {
   let bool = true
   let arrayOfRequiredFields = ['userId', 'title', 'companyId', 'description']
