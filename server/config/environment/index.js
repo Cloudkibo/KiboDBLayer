fa03c7af0065f314a49402b16aaf14de8665514c
anishachhatwani <i130315@nu.edu.pk>
2019-02-18 12:09:01 +0500

    scripts to normalize

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const _ = require('lodash')
 
 const all = {
-  env: process.env.NODE_ENV,
+  env: process.env.NODE_ENV || 'staging',
 
   secrets: {
     session: process.env.SESSION_SECRET || 'some string'
