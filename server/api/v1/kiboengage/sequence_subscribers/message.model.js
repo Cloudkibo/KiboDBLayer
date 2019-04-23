9c942921eed0fe0ec94bea7faf4efe96ac383ff1
Imran Shoukat <ishoukat@192.168.0.108>
2019-04-23 16:30:07 +0500

    sequence messaging changes

--- server/api/v1/kiboengage/sequence_subscribers/message.model.js
+++ server/api/v1/kiboengage/sequence_subscribers/message.model.js
@@ -7,7 +7,9 @@ const sequenceSubscribersMessages = new Schema({
   messageId: { type: Schema.ObjectId, ref: 'sequenceMessages' },
   companyId: { type: String },
   datetime: { type: Date, default: Date.now },
-  seen: {type: Boolean, default: false}
+  seen: {type: Boolean, default: false},
+  clicked: {type: Boolean, default: false},
+  received: {type: Schema.Types.Mixed}
 })
 
 module.exports = mongoose.model('sequenceSubscribersMessages', sequenceSubscribersMessages)
