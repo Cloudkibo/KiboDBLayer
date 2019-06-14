b69cfbbc2d559adaec6bc7df55410bb0c1ce870a
Muzamil Bukhari <muzamil.bukhari.39@gmail.com>
2019-06-14 12:32:50 +0500

    Tweets Queue model and end points

---
+++ server/api/v1/kiboengage/tweets_queue/tweets_queue.controller.js
@@ -0,0 +1,52 @@
+const Datalayer = require('./tweets_queue.datalayer')
+
+exports.index = function (req, res) {
+    Datalayer.findAllQueueObjects()
+    .then(foundObj => {
+        return res.status(200).json({status:'success', payload: foundObj})
+    })
+    .catch(err => {
+        return res.status(500).json({status:'failed', payload: err})
+    })
+}
+
+exports.create = function (req,res) {
+    console.log(req.body)
+    Datalayer.createOneQueueObject(req.body)
+    .then(resp => {
+        return res.status(200).json({status: 'success', payload: resp})
+    })
+    .catch(err => {
+        return res.status(500).json({status: 'failed', payload: err.toString()})
+    })
+}
+
+exports.delete = function(req, res){
+    Datalayer.deleteQueue(req.body)
+    .then(result => {
+        return res.status(200).json({status: 'success', payload: result})
+    })
+    .catch(err => {
+        return res.status(500).json({status: 'failed', payload: err})
+    })
+}
+
+exports.query = function(req, res){
+    Datalayer.findQueueUsingQuery(req.body)
+    .then(foundRes => {
+        return res.status(200).json({status: 'success', payload: foundRes})
+    })
+    .catch(err => {
+        return res.status(500).json({status: 'failed', payload: err})
+    })
+}
+
+exports.update = function(req, res){
+    Datalayer.findQueueUsingQuery(req.body)
+    .then(foundRes => {
+        return res.status(200).json({status: 'success', payload: foundRes})
+    })
+    .catch(err => {
+        return res.status(500).json({status: 'failed', payload: err})
+    })
+}
