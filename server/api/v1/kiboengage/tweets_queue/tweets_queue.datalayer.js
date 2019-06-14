140c14e8eb7d3dbd3f8caf558cddc8886a68c41a
Muzamil Bukhari <muzamil.bukhari.39@gmail.com>
2019-06-14 13:47:17 +0500

    Logic layer

--- server/api/v1/kiboengage/tweets_queue/tweets_queue.datalayer.js
+++ server/api/v1/kiboengage/tweets_queue/tweets_queue.datalayer.js
@@ -3,7 +3,7 @@ This file will contain the functions for data layer.
 By separating it from controller, we are separating the concerns.
 Thus we can use it from other non express callers like cron etc
 */
-//const LogicLayer = require('./seq_m_queue.logiclayer')
+const LogicLayer = require('./tweets_queue.logiclayer')
 const MongoInterface = require('./interface_mongo')
 const util = require('util')
 
