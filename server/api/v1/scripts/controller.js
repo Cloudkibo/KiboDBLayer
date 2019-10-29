eb93d9b6e0e893a39084f459b4f4ff01cdb191c6
Baqar <bjafri5@gmail.com>
2019-10-29 10:39:20 +0500

    changes

--- server/api/v1/scripts/controller.js
+++ server/api/v1/scripts/controller.js
@@ -818,21 +818,26 @@ exports.normalizeSequenceSubscribers = function (req, res) {
 }
 
 exports.normalizeClickCount = function (req, res) {
-  BroadcastsModel.find({}).exec()
+  BroadcastsModel.find({companyId: req.body.companyId}).exec()
     .then(broadcasts => {
+      let requests = []
       broadcasts.forEach(broadcast => {
         if (broadcast.clicks > broadcast.seen) {
-          BroadcastsModel.update({_id: broadcast._id}, {clicks: broadcast.seen}).exec()
-            .then(updated => {
-              logger.serverLog(TAG, `Updated broadcast click count for ${broadcast._id}`)
-            })
-            .catch(err => {
-              logger.serverLog(TAG, `Failed to update broadcast click count ${err}`)
-            })
+          requests.push(BroadcastsModel.update({_id: broadcast._id}, {clicks: broadcast.seen}).exec())
         }
       })
+      Promise.all(requests)
+        .then(results => {
+          logger.serverLog(TAG, `Updated all broadcast click counts for company ${req.body.companyId}`)
+          return res.status(200).json({status: 'success', payload: results})
+        })
+        .catch(err => {
+          logger.serverLog(TAG, `Failed to update broadcast click count ${err}`)
+          return res.status(500).json({status: `failed`, description: err})
+        })
     })
     .catch(err => {
       logger.serverLog(TAG, `Failed to fetch broadcasts ${err}`)
+      return res.status(500).json({status: `Failed to fetch broadcasts ${err}`, description: err})
     })
 }
