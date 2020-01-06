4750275c1678748b2ab6e72bd334e46c74c08517
anishachhatwani <i130315@nu.edu.pk>
2020-01-06 13:20:52 +0500

    rss feeds

--- server/api/v1/kiboengage/rssFeeds/rssFeeds.model.js
+++ server/api/v1/kiboengage/rssFeeds/rssFeeds.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const rssFeedsSchema = new Schema({
-  pageId: String,
+  pageIds: [],
   companyId: String,
   userId: String,
   feedUrl: String,
