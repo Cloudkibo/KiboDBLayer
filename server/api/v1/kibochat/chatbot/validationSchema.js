be9c0822f4e7c8b06cb43785fecf59aa97ebdaf8
Sojharo <sojharo@live.com>
2020-04-06 14:25:42 +0500

    Work on chatbot CloudKibo/KiboPush#8333

---
+++ server/api/v1/kibochat/chatbot/validationSchema.js
@@ -0,0 +1,73 @@
+/*
+This file will contain the validation schemas.
+By separating it from controller, we are cleaning the code.
+Now the middleware will automatically send error response if the payload fails
+*/
+// For express json validation
+exports.createPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'userId': {
+      'type': 'string'
+    },
+    'pageId': {
+      'type': 'string'
+    },
+    'companyId': {
+      'type': 'string'
+    }
+  },
+  'required': [
+    'userId',
+    'pageId',
+    'companyId'
+  ]
+}
+
+exports.queryPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string',
+      'required': true
+    },
+    'match': {
+      'type': 'object',
+      'required': true
+    }
+  }
+}
+
+exports.updatePayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string'
+    },
+    'match': {
+      'type': 'object',
+      'properties': {
+        'userId': {
+          'type': 'string'
+        },
+        'pageId': {
+          'type': 'string'
+        },
+        'companyId': {
+          'type': 'string'
+        }
+      }
+    },
+    'updated': {
+      'type': 'object'
+    }
+  },
+  'required': [
+    'purpose',
+    'match',
+    'updated'
+  ]
+}
