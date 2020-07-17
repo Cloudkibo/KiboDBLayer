02f49ebce64b1143ec75e61aa92a60ebebff31bc
anishachhatwani <i130315@nu.edu.pk>
2020-07-17 16:33:53 +0500

    whatsapp metrics

--- server/api/v1/kibochat/cannedResponses/validationSchema.js
+++ server/api/v1/kibochat/cannedResponses/validationSchema.js
@@ -5,66 +5,66 @@ Now the middleware will automatically send error response if the payload fails
 */
 // For express json validation
 exports.createPayload = {
-  '$schema': 'http://json-schema.org/draft-04/schema#',
-  'type': 'object',
-  'properties': {
-    'responseCode': {
-      'type': 'string'
-    },
-    'responseMessage': {
-      'type': 'string'
-    }
-  },
-  'required': [
-    'responseCode',
-    'responseMessage'
-  ]
-}
-
-exports.queryPayload = {
-  '$schema': 'http://json-schema.org/draft-04/schema#',
-  'type': 'object',
-  'properties': {
-    'purpose': {
-      'type': 'string',
-      'required': true
-    },
-    'match': {
-      'type': 'object',
-      'required': true
-    }
-  }
-}
-
-exports.updatePayload =
-{
-  '$schema': 'http://json-schema.org/draft-04/schema#',
-  'type': 'object',
-  'properties': {
-    'purpose': {
-      'type': 'string'
-    },
-    'match': {
-      'type': 'object',
-      'properties': {
-        '_id': {
-          'type': 'string'
-        },
-        'responseCode': {
-          'type': 'string'
-        },
-        'responseMessage': {
-          'type': 'string'
-        }
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'object',
+    'properties': {
+      'responseCode': {
+        'type': 'string'
+      },
+      'responseMessage': {
+        'type': 'string'
       }
     },
-    'updated': {
-      'type': 'object'
+    'required': [
+      'responseCode',
+      'responseMessage'
+    ]
+  }
+
+  exports.queryPayload = {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'object',
+    'properties': {
+      'purpose': {
+        'type': 'string',
+        'required': true
+      },
+      'match': {
+        'type': 'object',
+        'required': true
+      }
     }
-  },
-  'required': [
-    'purpose',
-    'match',
-    'updated'
-  ]
-}
+  }
+
+  exports.updatePayload =
+  {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'object',
+    'properties': {
+      'purpose': {
+        'type': 'string'
+      },
+      'match': {
+        'type': 'object',
+        'properties': {
+          '_id': {
+            'type': 'string'
+          },
+          'responseCode': {
+            'type': 'string'
+          },
+          'responseMessage': {
+            'type': 'string'
+          }
+        }
+      },
+      'updated': {
+        'type': 'object'
+      }
+    },
+    'required': [
+      'purpose',
+      'match',
+      'updated'
+    ]
+  }
