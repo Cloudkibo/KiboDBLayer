419b9deb8ae47b61fb4def2486d5be1f2f8bbf0f
Baqar <bjafri5@gmail.com>
2020-08-25 15:43:40 +0500

    fix

--- server/api/v1/kibochat/whatsAppChatbot/whatsAppChatbot.model.js
+++ server/api/v1/kibochat/whatsAppChatbot/whatsAppChatbot.model.js
@@ -8,19 +8,19 @@ let WhatsAppChatbotSchema = new Schema({
   datetime: { type: Date, default: Date.now },
   usedBy: { type: Number, default: 0 },
   triggers: { type: Array, default: ['Hi', 'Hello'] },
-  botLinks: new Schema({
+  botLinks: {
     faqs: { type: String, default: '' },
     paymentMethod: { type: String, default: '' },
     returnPolicy: { type: String, default: '' }
-  }),
+  },
   testSubscribers: { type: Array, default: [] },
   startingBlockId: String,
   maxLevels: { type: Number, default: 3 },
   published: { type: Boolean, default: false },
-  stats: new Schema({
+  stats: {
     triggerWordsMatched: { type: Number, default: 0 },
     newSubscribers: { type: Number, default: 0 }
-  })
+  }
 })
 
 module.exports = mongoose.model('whatsapp_chatbot', WhatsAppChatbotSchema)
