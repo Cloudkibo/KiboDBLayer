4bc8f5a462015bea46764d03c65caaf62d6477c7
anishachhatwani <i130315@nu.edu.pk>
2020-12-09 15:15:01 +0500

    crud for broadcast responses

---
+++ server/api/v1/kiboengage/broadcastResponses/validationSchema.js
@@ -0,0 +1,65 @@
+exports.createPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'companyId': {
+      'type': 'string'
+    },
+    'broadcastId': {
+      'type': 'string'
+    },
+    'customerId': {
+      'type': 'string'
+    },
+    'platform': {
+      'type': 'string'
+    },
+    'response': {
+      'type': 'object'
+    }
+  },
+  'required': [
+    'platform',
+    'response',
+    'customerId',
+    'broadcastId',
+    'companyId'
+  ]
+}
+
+exports.queryPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string',
+      'required': true
+    },
+    'match': {
+      'type': 'object',
+      'required': true
+    }
+  }
+}
+
+exports.updatePayload =
+{
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string'
+    },
+    'match': {
+      'type': 'object'
+    },
+    'updated': {
+      'type': 'object'
+    }
+  },
+  'required': [
+    'purpose',
+    'match',
+    'updated'
+  ]
+}
