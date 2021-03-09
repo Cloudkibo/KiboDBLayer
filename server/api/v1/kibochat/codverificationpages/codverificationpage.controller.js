9a0313a928b6548cc02f34f809de2ae4b9338006
Sojharo <sojharo@live.com>
2021-03-09 17:17:47 +0500

    db work cod CloudKibo/KiboPush#11114

---
+++ server/api/v1/kibochat/codverificationpages/codverificationpage.controller.js
@@ -0,0 +1,66 @@
+// Web layer of this API node
+const logger = require('../../../../components/logger')
+const DataLayer = require('./codverificationpage.datalayer')
+const TAG = '/api/v1/kibochat/codverificationpage/codverificationpage.controller.js'
+const { sendSuccessResponse, sendErrorResponse } = require('../../../../global/response')
+// const util = require('util')
+
+exports.index = function (req, res) {
+  DataLayer.findAllCodPageObjects()
+    .then(foundObjects => {
+      sendSuccessResponse(res, 200, foundObjects)
+    })
+    .catch(err => {
+      const message = err || 'Failed to find all codpages'
+      logger.serverLog(message, `${TAG}: exports.index`, req.body, {}, 'error')
+      sendErrorResponse(res, 500, err.toString())
+    })
+}
+
+exports.create = function (req, res) {
+  DataLayer.createOneCodPageObject(req.body)
+    .then(createdObject => {
+      sendSuccessResponse(res, 200, createdObject)
+    })
+    .catch(err => {
+      const message = err || 'Failed to create cod page'
+      logger.serverLog(message, `${TAG}: exports.create`, req.body, {}, 'error')
+      sendErrorResponse(res, 500, err.toString())
+    })
+}
+
+exports.query = function (req, res) {
+  DataLayer.findCodPageUsingQuery(req.body)
+    .then(foundObjects => {
+      sendSuccessResponse(res, 200, foundObjects)
+    })
+    .catch(err => {
+      const message = err || 'Failed to find codpages'
+      logger.serverLog(message, `${TAG}: exports.query`, req.body, {}, 'error')
+      sendErrorResponse(res, 500, err.toString())
+    })
+}
+
+exports.update = function (req, res) {
+  DataLayer.updateCodPages(req.body)
+    .then(foundObjects => {
+      sendSuccessResponse(res, 200, foundObjects)
+    })
+    .catch(err => {
+      const message = err || 'Failed to update code page'
+      logger.serverLog(message, `${TAG}: exports.update`, req.body, {}, 'error')
+      sendErrorResponse(res, 500, err.toString())
+    })
+}
+
+exports.delete = function (req, res) {
+  DataLayer.deleteCodPages(req.body)
+    .then(result => {
+      sendSuccessResponse(res, 200, result)
+    })
+    .catch(err => {
+      const message = err || 'Failed to delete code page'
+      logger.serverLog(message, `${TAG}: exports.delete`, req.body, {}, 'error')
+      sendErrorResponse(res, 500, err.toString())
+    })
+}
---
+++ server/api/v1/kibochat/codverificationpages/validationSchema.js
@@ -0,0 +1,72 @@
+/*
+This file will contain the validation schemas.
+By separating it from controller, we are cleaning the code.
+Now the middleware will automatically send error response if the payload fails
+*/
+// For express json validation
+exports.createPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'companyId': {
+      'type': 'string'
+    },
+    'contactId': {
+      'type': 'object'
+    },
+    'order': {
+      'type': 'object'
+    },
+    'storeName': {
+      'type': 'object'
+    },
+    'storeType': {
+      'type': 'object'
+    }
+  },
+  'required': [
+    'companyId', 'contactId', 'order', 'storeName', 'storeType'
+  ]
+}
+
+exports.queryPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string',
+      'required': true
+    },
+    'match': {
+      'type': 'object',
+      'required': true
+    }
+  }
+}
+
+exports.updatePayload =
+{
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'purpose': {
+      'type': 'string'
+    },
+    'match': {
+      'type': 'object',
+      'properties': {
+        'companyId': {
+          'type': 'string'
+        }
+      }
+    },
+    'updated': {
+      'type': 'object'
+    }
+  },
+  'required': [
+    'purpose',
+    'match',
+    'updated'
+  ]
+}
