f6f1a4a1f67b8b619963d33698a59ad906464e37
AnishaChhatwani <i130315@nu.edu.pk>
2021-03-17 14:46:21 +0500

    super number v2

--- server/api/v1/kibochat/superNumberMessageLogs/logiclayer.js
+++ server/api/v1/kibochat/superNumberMessageLogs/logiclayer.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose')
+
 exports.prepareMongoAggregateQuery = (body) => {
   let query = []
   if (body.project) {
@@ -8,16 +10,34 @@ exports.prepareMongoAggregateQuery = (body) => {
       body.match.datetime.$gte = new Date(body.match.datetime.$gte)
       body.match.datetime.$lt = new Date(body.match.datetime.$lt)
       query.push({$match: body.match})
-    } else query.push({$match: body.match})
+    }
+    if (body.match._id) {
+      if (body.match._id.$lt) {
+        body.match._id.$lt = mongoose.Types.ObjectId(body.match._id.$lt)
+      }
+      if (body.match._id.$gt) {
+        body.match._id.$gt = mongoose.Types.ObjectId(body.match._id.$gt)
+      }
+    }
+    if (body.match.$and) {
+      body.match.$and.forEach((obj, index) => {
+        if (obj._id && obj._id.$gt) {
+          body.match.$and[index]._id.$gt = mongoose.Types.ObjectId(body.match.$and[index]._id.$gt)
+        }
+        if (obj._id && obj._id.$lt) {
+          body.match.$and[index]._id.$lt = mongoose.Types.ObjectId(body.match.$and[index]._id.$lt)
+        }
+      })
+    }
+    query.push({$match: body.match})
   } else return 'Match Criteria Not Found'
 
   if (body.group) {
     if (!Object.keys(body.group).includes('_id')) return '_id is missing in Group Criteria'
     else query.push({$group: body.group})
   }
-
-  if (body.skip) query.push({$skip: body.skip})
   if (body.sort) query.push({$sort: body.sort})
+  if (body.skip) query.push({$skip: body.skip})
   if (body.limit) query.push({$limit: body.limit})
 
   return query
