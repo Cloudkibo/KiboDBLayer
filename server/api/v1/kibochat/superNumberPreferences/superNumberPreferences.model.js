c70b82dbf10a931535b29cadb276ba89756afcf4
sania <sania@cloudkibo.com>
2021-03-23 20:55:24 +0500

    update schema super number preferences

--- server/api/v1/kibochat/superNumberPreferences/superNumberPreferences.model.js
+++ server/api/v1/kibochat/superNumberPreferences/superNumberPreferences.model.js
@@ -68,6 +68,58 @@ let SuperNumberPreferences = new Schema({
       accountPages: {type: Boolean, default: false},
       urlsEndinginPages: {type: Boolean, default: false}
     }
+  },
+  chat_widget: {
+    enabled: {type: Boolean, default: true},
+    agentsLimit: {type: Number, default: 6},
+    agents: { type: Array },
+    btnDesign: {
+      backgroundColorStyle: {type: String},
+      backgroundColor1: {type: String}, // hex color code
+      backgroundColor2: {type: String}, // only set when backgroundColorStyle is gradient
+      iconColor: {type: String}, // hex color code
+      btnTextColor: {type: String} // hex color code
+    },
+    textMessage: {
+      shareBtnText: {type: String, default: 'Chat with us'},
+      shareMessage: {type: String, default: `I'm interested in this product and I have a few questions. Can you help?`},
+      includePageURL: {type: Boolean, default: true}
+    },
+    callOutCard: {
+      enabled: {type: Boolean, default: true},
+      cardText: {type: String},
+      cardDelay: {type: Number, default: 5}
+    },
+    greetingsWidget: {
+      backgroundColorStyle: {type: String},
+      backgroundColor1: {type: String}, // hex color code
+      backgroundColor2: {type: String}, // only set when backgroundColorStyle is gradient
+      headingColor: {type: String}, // hex color code
+      descriptionColor: {type: String} // hex color code
+    },
+    titleText: {type: String, default: 'Hi there ?'},
+    helpText: {type: String, default: 'We are here to help. Chat with us on WhatsApp for any queries.'},
+    offlineStoreMsg: {type: String, default: '“Hi, our working hours are <start time> to <end time>, request you to reach us at the same time. Apologies for the inconvenience.'},
+    randomAgentsOrder: {type: Boolean, default: true},
+    displayPosition: {
+      display: {type: String, default: 'both'},
+      mobilePosition: {type: String, default: 'right'},
+      desktopPosition: {type: String, default: 'right'},
+      desktopHeightOffset: {type: Number, default: 20},
+      desktopEdgeOffset: {type: Number, default: 20},
+      mobileHeightOffset: {type: Number, default: 20},
+      mobileEdgeOffset: {type: Number, default: 20}
+    },
+    displayPages: {
+      homePage: {type: Boolean, default: true},
+      collectionsPage: {type: Boolean, default: true},
+      productPages: {type: Boolean, default: true},
+      cartPageDesktop: {type: Boolean, default: true},
+      cartPageMobile: {type: Boolean, default: false},
+      blogPostPages: {type: Boolean, default: true},
+      accountPages: {type: Boolean, default: true},
+      urlsEndinginPages: {type: Boolean, default: true}
+    }
   }
 })
 
